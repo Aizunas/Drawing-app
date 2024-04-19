60b0dcb66703739d16c8e99c5711a518bb5cf56a
itsjayn3 <148452730+itsjayn3@users.noreply.github.com>
2024-04-19 17:46:43 +0100

    Update squareTool.js

--- squareTool.js
+++ squareTool.js
@@ -1,9 +1,9 @@
 function squareTool () {
-  this.name = "Square" ; 
+  this.name = "square" ; 
   this.icon = "assets/square.jpg";
 
-  var previousMouseX = -1;
-	var previousMouseY = -1;
+var previousMouseX = -1;
+var previousMouseY = -1;
 
   this.draw = function(){
     updatePixels();
@@ -14,21 +14,36 @@ function squareTool () {
     //check if they previousX and Y are -1. set them to the current
 			//mouse X and Y if they are.
 			if (previousMouseX == -1){
-				previousMouseX = mouseX;
-				previousMouseY = mouseY;
+			previousMouseX = mouseX;
+			previousMouseY = mouseY;
 			}
 			//if we already have values for previousX and Y we can draw a line from 
 			//there to the current mouse location
-			else{
-				line(previousMouseX, previousMouseY, mouseX, mouseY);
-				previousMouseX = mouseX;
-				previousMouseY = mouseY;
-			}
+			// Otherwise, erase the line between the previous and current mouse positions
+            else {
+                stroke(255); // Set stroke color to white (erase color)
+                strokeWeight(10); // Set stroke weight to a larger value for erasing
+                line(previousMouseX, previousMouseY, mouseX, mouseY);
+                previousMouseX = mouseX;
+                previousMouseY = mouseY;
+	    
 
-      else{
-			previousMouseX = -1;
-			previousMouseY = -1;
-		} 
+}else{
+	previousMouseX = -1;
+	previousMouseY = -1;
+        } 
+
+	loadPixels();
+  };
+	// Reset the tool state when it's deselected
+    this.unselectTool = function() {
+        // Reset previous mouse coordinates
+        previousMouseX = -1;
+        previousMouseY = -1;
+    };
+	 this.populateOptions = function() {
+        select(".options").html("");
+	 };
     }
 
   
