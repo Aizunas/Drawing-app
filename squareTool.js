c7270a83600bfa4193657646a70a79634b8a5161
itsjayn3 <148452730+itsjayn3@users.noreply.github.com>
2024-04-19 17:51:16 +0100

    Update squareTool.js

--- squareTool.js
+++ squareTool.js
@@ -1,49 +1,47 @@
-function squareTool () {
-  this.name = "square" ; 
-  this.icon = "assets/square.jpg";
+function squareTool() {
+    this.name = "square";
+    this.icon = "assets/square.jpg";
 
-var previousMouseX = -1;
-var previousMouseY = -1;
+    // Mouse coordinates for the previous draw
+    var previousMouseX = -1;
+    var previousMouseY = -1;
+   
+    this.draw = function() {
+        // Display the last saved state of pixels
+        updatePixels();
 
-  this.draw = function(){
-    updatePixels();
-
-    // Draw the square when mouse is pressed 
-
-    if(mouseIsPressed){
-    //check if they previousX and Y are -1. set them to the current
-			//mouse X and Y if they are.
-			if (previousMouseX == -1){
-			previousMouseX = mouseX;
-			previousMouseY = mouseY;
-			}
-			//if we already have values for previousX and Y we can draw a line from 
-			//there to the current mouse location
-			// Otherwise, erase the line between the previous and current mouse positions
+        // Erase the drawing if the mouse is pressed
+        if (mouseIsPressed) {
+            // If the previous values are -1, set them to the current mouse location
+            if (previousMouseX == -1) {
+                previousMouseX = mouseX;
+                previousMouseY = mouseY;
+            }
+            // Otherwise, erase the line between the previous and current mouse positions
             else {
                 stroke(255); // Set stroke color to white (erase color)
                 strokeWeight(10); // Set stroke weight to a larger value for erasing
                 line(previousMouseX, previousMouseY, mouseX, mouseY);
                 previousMouseX = mouseX;
                 previousMouseY = mouseY;
-	    
+            }
+        } else {
+            // Reset previous mouse coordinates
+            previousMouseX = -1;
+            previousMouseY = -1;
+        }
 
-}else{
-	previousMouseX = -1;
-	previousMouseY = -1;
-        } 
+        // After the drawing is done, save the pixel state
+        loadPixels();
+    };
 
-	loadPixels();
-  };
-	// Reset the tool state when it's deselected
+    // Reset the tool state when it's deselected
     this.unselectTool = function() {
         // Reset previous mouse coordinates
         previousMouseX = -1;
         previousMouseY = -1;
     };
-	 this.populateOptions = function() {
-        select(".options").html("");
-	 };
-    }
 
-  
+    // No options for rubber tool
+    this.populateOptions = function() {
+        select(".options").html("");
