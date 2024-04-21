df6b31718afb59fe12b76a99fced7aab714c980a
Aizunas <148207974+Aizunas@users.noreply.github.com>
2024-04-21 21:11:31 +0100

    Update squareTool.js

--- squareTool.js
+++ squareTool.js
@@ -2,44 +2,93 @@ function squareTool() {
     this.name = "square";
     this.icon = "assets/square.jpg";
 
-    // Array to store the squares
-    var squares = [];
+	var startMouseX = -1;
+	var startMouseY = -1;
+	var drawing = false;
+    
+    var noFillButton;
+    var self = this;
+    
+    this.populateOptions = function() {
+        if(self.name == "square"){
+            //Created a button for a no fill option
+            select(".options").html(
+                "<button id = 'noFillButton'> No Fill </button>");
 
-    // Mouse coordinates for the square being drawn
-    var startX = -1;
-    var startY = -1;
+            select("#noFillButton").mouseClicked(function(){
+                noFillButton = select("#" + this.elt.id);
 
-    this.draw = function() {
-        // Draw all the previously drawn squares
-        for (var i = 0; i < squares.length; i++) {
-            var square = squares[i];
-            noFill();
-            rect(square.startX, square.startY, square.width, square.height);
+                //Starts off with the button saying "no fill", and draws a rect with a fill colour.
+                if(colourP.fillMode == true) {
+                    fill(colourP.selectedColour)
+                    colourP.fillMode = false;
+                    noFillButton.html("No Fill");
+
+                //When the no fill button is pressed, the button will change to say "fill" instead and have no fill when drawing a rect
+                } else {
+                    noFill();
+                    colourP.fillMode = true;
+                    noFillButton.html("Fill");
+                }
+            });
         }
+        
+        helpers.slider();
+	};
+    
+    this.unselectTool = function() {
+        if(self.name == "square"){
+            //clears the options section
+            select(".options").html("");
 
-        // If the mouse is pressed, record the starting position of the new square
-        if (mouseIsPressed) {
-            if (startX == -1 && startY == -1) {
-                startX = mouseX;
-                startY = mouseY;
-            } else {
-                // Clear the canvas
-                clear();
-                
-                // Draw the square outline as the mouse moves
-                noFill(); // Draw only the outline
-                rect(startX, startY, mouseX - startX, mouseY - startY);
-                
-            }
-        } else {
-            // If the mouse is released, calculate the dimensions of the new square
-            if (startX != -1 && startY != -1) {
-                var width = mouseX - startX;
-                var height = mouseY - startY;
-                squares.push({ startX: startX, startY: startY, width: width, height: height });
-                startX = -1;
-                startY = -1;
-            }
+            //Reset the mode for the button to work correctly after switching tools
+            colourP.fillMode = false;
         }
-    };
+        
+        //Change the cursor back to an arrow
+        helpers.arrow();
+	}
+
+	//draws the line to the screen 
+	this.draw = function(){
+        
+        //Change the cursor to a cross
+        helpers.cross();
+
+		//only draw when mouse is clicked
+		if(mouseIsPressed){
+			//if it's the start of drawing a new line
+			if(startMouseX == -1){
+				startMouseX = mouseX;
+				startMouseY = mouseY;
+				drawing = true;
+				//save the current pixel Array
+				loadPixels();
+			}
+
+			else{
+				//update the screen with the saved pixels to hide any previous
+				//line between mouse pressed and released
+				updatePixels();
+                
+                if(self.name == "square"){
+                    //draw the rect
+				    rect(startMouseX, startMouseY, mouseX-startMouseX, mouseY-startMouseY);
+                }
+                else{
+                    //draw the line
+                    line(startMouseX, startMouseY, mouseX, mouseY);
+                }
+			}
+		}
+
+		else if(drawing){
+			//save the pixels with the most recent line and reset the
+			//drawing bool and start locations
+			loadPixels();
+			drawing = false;
+			startMouseX = -1;
+			startMouseY = -1;
+		}
+	};
 }
