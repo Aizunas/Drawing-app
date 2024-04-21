3ae1db75f192ac1a7104e6c1ad7e28ec836d0ab9
Aizunas <148207974+Aizunas@users.noreply.github.com>
2024-04-21 20:44:58 +0100

    Update squareTool.js

--- squareTool.js
+++ squareTool.js
@@ -2,46 +2,44 @@ function squareTool() {
     this.name = "square";
     this.icon = "assets/square.jpg";
 
-    // Mouse coordinates for the previous draw
-    var previousMouseX = -1;
-    var previousMouseY = -1;
-   
-    this.draw = function() {
-        // Display the last saved state of pixels
-        updatePixels();
+    // Array to store the squares
+    var squares = [];
 
-        // Erase the drawing if the mouse is pressed
-        if (mouseIsPressed) {
-            // If the previous values are -1, set them to the current mouse location
-            if (previousMouseX == -1) {
-                previousMouseX = mouseX;
-                previousMouseY = mouseY;
-            }
-            // Otherwise, erase the line between the previous and current mouse positions
-            else {
-                stroke(255); // Set stroke color to white (erase color)
-                strokeWeight(10); // Set stroke weight to a larger value for erasing
-                line(previousMouseX, previousMouseY, mouseX, mouseY);
-                previousMouseX = mouseX;
-                previousMouseY = mouseY;
-            }
-        } else {
-            // Reset previous mouse coordinates
-            previousMouseX = -1;
-            previousMouseY = -1;
+    // Mouse coordinates for the square being drawn
+    var startX = -1;
+    var startY = -1;
+
+    this.draw = function() {
+        // Draw all the previously drawn squares
+        for (var i = 0; i < squares.length; i++) {
+            var square = squares[i];
+            noFill();
+            rect(square.startX, square.startY, square.width, square.height);
         }
 
-        // After the drawing is done, save the pixel state
-        loadPixels();
+        // If the mouse is pressed, record the starting position of the new square
+        if (mouseIsPressed) {
+            if (startX == -1 && startY == -1) {
+                startX = mouseX;
+                startY = mouseY;
+            } else {
+                // Clear the canvas
+                clear();
+                
+                // Draw the square outline as the mouse moves
+                noFill(); // Draw only the outline
+                rect(startX, startY, mouseX - startX, mouseY - startY);
+                
+            }
+        } else {
+            // If the mouse is released, calculate the dimensions of the new square
+            if (startX != -1 && startY != -1) {
+                var width = mouseX - startX;
+                var height = mouseY - startY;
+                squares.push({ startX: startX, startY: startY, width: width, height: height });
+                startX = -1;
+                startY = -1;
+            }
+        }
     };
-
-    // Reset the tool state when it's deselected
-    this.unselectTool = function() {
-        // Reset previous mouse coordinates
-        previousMouseX = -1;
-        previousMouseY = -1;
-    };
-
-    // No options for square tool
-    this.populateOptions = function() {
-        select(".options").html("");
+}
